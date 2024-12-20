968ef3eac4fb6bffaf1839a0aa29fa340fcb2106
ahpriansyahgery <ahpriansyahgery9c@gmail.com>
2024-12-20 15:16:01 +0700

    install Breeze

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,8 @@ Route::get('succesCheckout', function () {
 })->name('succesCheckout');
 
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
